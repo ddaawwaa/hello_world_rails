d3229c1b211190c05d1ed318587fdbb44c007ff2
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-07-13 23:38:40 +0900

    Add user#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    binding.pry
+    @user = User.find(params[:id])
+    @user.update!(user_params)
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:account, :name, :email)
+    end
 end
