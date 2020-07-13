8bc1725f6b2245ca3ac0977b732d1b91c02d32f3
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-07-13 23:45:02 +0900

    refactaring users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def index
     @users = User.all
     render json: @users
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update!(user_params)
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy!
   end
 
 
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:account, :name, :email)
     end
