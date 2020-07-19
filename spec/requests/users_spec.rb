278fef3612c8e24613cd2bcfe393af38db7267c1
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-07-19 21:56:31 +0900

    Add users#index request test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "ユーザーの一覧が取得できる" do
+    subject { get(users_path) }
+    bofore do
+      create_list(:user, 3)
+    end
 
+    it "ユーザーの一覧が取得できる" do
+      subject
+      res = JSON.parse(response.body)
+      expect(res.length).to eq 3
+      expect(res[0].keys).to eq ["account", "name", "email"]
+      expect(response).to have_http_status(200)
     end
   end
   describe "GET /users/:id" do
