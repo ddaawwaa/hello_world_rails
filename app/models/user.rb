129d5252a0aff0b8feda355ff6c27c7d173afb03
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-07-26 00:37:57 +0900

    make user_detail table and associate to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :account, uniqueness: true
 
   has_many :articles
-  # has_one :user_detail
+  has_one :user_detail
   # has_many :user_communities
   # has_many :communities, through::user_communities
 end
